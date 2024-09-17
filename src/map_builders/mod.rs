03f0f129d5fa22267590ed1910e6c559adf81106
P Moore <perry.moore@gmail.com>
2024-09-17 21:00:33 -0230

    4.8 (DLA map gen) complete

--- src/map_builders/mod.rs
+++ src/map_builders/mod.rs
@@ -14,6 +14,8 @@ mod random_walk;
 use random_walk::DrunkardsWalkBuilder;
 mod maze;
 use maze::MazeBuilder;
+mod dla;
+use dla::DLABuilder;
 
 mod common;
 use common::*;
@@ -32,18 +34,22 @@ pub trait MapBuilder {
 pub fn random_builder(new_depth: i32) -> Box<dyn MapBuilder> {
     // Note that until we have a second map type, this isn't even slightly random
     let mut rng = rltk::RandomNumberGenerator::new();
-    let type_roll = rng.roll_dice(1, 1) + 0;
-    match type_roll {
-        1 => Box::new(MazeBuilder::new(new_depth)),
-        2 => Box::new(DrunkardsWalkBuilder::winding_passages(new_depth)),
-        3 => Box::new(DrunkardsWalkBuilder::open_halls(new_depth)),
+    let builder = rng.roll_dice(1, 4) + 7;
+    match builder {
+        1 => Box::new(BspDungeonBuilder::new(new_depth)),
+        2 => Box::new(BspInteriorBuilder::new(new_depth)),
+        3 => Box::new(CellularAutomataBuilder::new(new_depth)),
         4 => Box::new(DrunkardsWalkBuilder::open_area(new_depth)),
-        5 => Box::new(CellularAutomataBuilder::new(new_depth)),
-        6 => Box::new(BspInteriorBuilder::new(new_depth)),
-        7 => Box::new(BspDungeonBuilder::new(new_depth)),
-        8 => Box::new(RubbleMapBuilder::new(new_depth)),
+        5 => Box::new(DrunkardsWalkBuilder::open_halls(new_depth)),
+        6 => Box::new(DrunkardsWalkBuilder::winding_passages(new_depth)),
+        7 => Box::new(MazeBuilder::new(new_depth)),
+        8 => Box::new(DLABuilder::walk_inwards(new_depth)),
+        9 => Box::new(DLABuilder::walk_outwards(new_depth)),
+        10 => Box::new(DLABuilder::central_attractor(new_depth)),
+        11 => Box::new(DLABuilder::insectoid(new_depth)),
+        12 => Box::new(RubbleMapBuilder::new(new_depth)),
         _ => Box::new(SimpleMapBuilder::new(new_depth)),
     }
-
-    //Box::new(SimpleMapBuilder::new(new_depth))
 }
+
+//Box::new(SimpleMapBuilder::new(new_depth))
