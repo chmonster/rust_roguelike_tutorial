9a700dc5db6ebf50d957f72051cc993bd3cdbcdb
P Moore <perry.moore@gmail.com>
2024-09-22 19:48:39 -0230

    debugging wasm-bindgen

--- build_wasm.sh
+++ build_wasm.sh
@@ -2,4 +2,4 @@
 echo "cargo build"
 cargo build --release --target wasm32-unknown-unknown
 echo "wasm-bindgen"
-wasm-bindgen ./target/wasm32-unknown-unknown/release/rust_roguelike.wasm --out-dir wasm --no-typescript
+wasm-bindgen ./target/wasm32-unknown-unknown/release/rust_roguelike.wasm --out-dir wasm --no-modules --no-typescript
